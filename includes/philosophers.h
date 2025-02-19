9f6ff8b8f5c37b598054a0be0f2db423778abaa9
ksudyn <ksudyn@student.42madrid.com>
2025-02-19 19:43:16 +0100

    philo

---
+++ includes/philosophers.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ksudyn <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/19 19:12:54 by ksudyn            #+#    #+#             */
+/*   Updated: 2025/02/19 19:21:59 by ksudyn           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <sys/time.h>//gettimeofday
+
+# include <pthread.h>
+//pthread_create pthread_detach pthread_join pthread_mutex_init
+//pthread_mutex_destroy pthread_mutex_lock pthread_mutex_unlock
+
+# include <unistd.h>//write usleep
+
+# include <stdio.h>//printf
+
+# include <stdlib.h>//malloc free
+
+# include <string.h>//memset
+
+#endif
