0a5b41d594d9503aa7283c2abf1df04bf280228d
ksudyn <ksudyn@student.42madrid.com>
2025-03-07 20:26:09 +0100

    philosophos

--- src/number_of_philosophers.c
+++ src/number_of_philosophers.c
@@ -21,5 +21,23 @@ int		philos(t_parse	*philo)
     return (ft_error(3));
 
 	i = 0;
-	
+	  while (i < philo->num_philo) {
+        philo->philos[i].philo = i + 1;  // Asigna el ID del filósofo
+        philo->philos[i].parse = philo;  // Conecta con la estructura principal
+        philo->philos[i].last_eat = 0;   // Inicializa el tiempo de última comida
+        philo->philos[i].act_time = 0;   // Inicializa el tiempo actual
+
+        // Inicializar el tenedor derecho
+        pthread_mutex_init(&philo->philos[i].right_fork, NULL);
+
+        // Asignar el tenedor izquierdo (el derecho del anterior)
+        if (i == 0)
+            philo->philos[i].left_fork = &philo->philos[philo->num_philo - 1].right_fork;
+        else
+            philo->philos[i].left_fork = &philo->philos[i - 1].right_fork;
+
+        i++;
+    }
+
+    return (0);
 }
